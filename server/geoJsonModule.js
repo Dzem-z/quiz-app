396fcc2bcbfab36f129a416994ec970818a07a6c
Dzem-z <egneradam@gmail.com>
2024-01-05 17:59:08 +0100

    added multipolygons handling to geoJSONparser

--- server/geoJsonModule.js
+++ server/geoJsonModule.js
@@ -1,38 +1,70 @@
 const osmtogeojson = require('osmtogeojson');
 
 geoJSONparser = {
-    parse : function(geoJson){
-        return osmtogeojson(geoJson);
+    parse : function(json){
+        return osmtogeojson(json);
     },
 
     simplifyGeometry : function(geoJson, optimizationFactor){
         console.log("entered--");
         let start = Date.now();
-        pointsLeft = new Set([]);   //all points that will not be optimized out
         if(geoJson.features != undefined) {
+            pointsLeft = new Set([]);   //all points that will not be optimized out
+            nPoints = new Map();
+
+
             for(let feature of geoJson.features){
-                if(feature.geometry.type == 'Polygon'){
-                    let iter = 0;
-                    for(let point of feature.geometry.coordinates[0]){
-                        if(iter >= optimizationFactor){
-                            iter = 0;
-                            pointsLeft.add(point);
+                if(feature.geometry.type == 'Polygon' || feature.geometry.type == 'MultiPolygon'){
+                    /**
+                    * counts multipolygonal points (points that belong to more than 2 polygons) and evenly removes most of the points.
+                    * the points that remain are roughly in the proportion 1/(optimizationFactor) to all points before optimization.
+                    */
+                    for(polygonCordinates of feature.geometry.coordinates){
+                        let firstStringifiedPoint = String(feature.geometry.coordinates[0][0]);
+                        //count points
+                        if(nPoints.has())
+                            nPoints.set(firstStringifiedPoint, nPoints.get(firstStringifiedPoint) + 1);
+                        else
+                            nPoints.set(firstStringifiedPoint, 1);
+                
+                        //add connecting point
+                        pointsLeft.add(firstStringifiedPoint);
+                        let iter = 0;
+                        for(let point of polygonCordinates){
+                            let stringifiedPoint = String(point);
+                            if(nPoints.has(stringifiedPoint))
+                                nPoints.set(stringifiedPoint, nPoints.get(stringifiedPoint) + 1);
+                            else
+                                nPoints.set(stringifiedPoint, 1);
+                            if(iter >= optimizationFactor){
+                                iter = 0;
+                                pointsLeft.add(stringifiedPoint);
+                            }
+                            iter++;
                         }
-                        iter++;
                     }
+                }
+            }
+            //add nPoints where n > 2
+            console.log(nPoints.entries());
+            for(let point of nPoints) {
+                if(point[1] > 2){
+                    console.log("mPoint: " + point[0] +"num:" + point[1]);
+                    pointsLeft.add(point[0]);
                 }
             }
 
             for(let feature of geoJson.features){
-                if(feature.geometry.type == 'Polygon'){
-                    let coordinates = feature.geometry.coordinates[0];
-                    feature.geometry.coordinates = [[]];
-                    for(let point of coordinates){
-                        if(pointsLeft.has(point)){
-                            feature.geometry.coordinates[0].push(point);
+                if(feature.geometry.type == 'Polygon' || feature.geometry.type == 'MultiPolygon'){
+                    for(polygons of feature.geometry.coordinates){
+                        let coordinates = polygons[0];
+                        polygons = [[]];
+                        for(let point of coordinates){
+                            if(pointsLeft.has(String(point))){
+                                polygons[0].push(point);
+                            }
                         }
                     }
-                    console.log(feature.geometry.coordinates[0].length/coordinates.length);
                 }
             }
         }
@@ -45,7 +77,7 @@ geoJSONparser = {
             let featuresOld = geoJson.features;
             geoJson.features = [];
             for(let feature of featuresOld){
-                if(feature.geometry.type == 'Polygon'){
+                if(feature.geometry.type == 'Polygon' || feature.geometry.type == 'MultiPolygon'){
                     geoJson.features.push(feature);
                 }
             }
