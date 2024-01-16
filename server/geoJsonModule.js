7cf898022be956cf16d85d6253215e67297cfd9d
Dzem-z <egneradam@gmail.com>
2024-01-16 21:15:22 +0100

    bug fixes

--- server/geoJsonModule.js
+++ server/geoJsonModule.js
@@ -19,9 +19,14 @@ geoJSONparser = {
 
             for(let feature of geoJson.features){
                 if(feature.geometry.type == 'Polygon' || feature.geometry.type == 'MultiPolygon'){
+                    let encapsulate;
+                    if(feature.geometry.type == 'Polygon')
+                        encapsulate = [feature.geometry.coordinates];
+                    else
+                        encapsulate = feature.geometry.coordinates;
                     // counts multipolygonal points (points that belong to more than 2 polygons) and leaves them untouched.
-                    for(polygonCordinates of feature.geometry.coordinates){
-                        let firstStringifiedPoint = String(feature.geometry.coordinates[0][0]);
+                    for(polygonCordinates of encapsulate){
+                        let firstStringifiedPoint = String(polygonCordinates[0][0]);
                         //count points
                         if(nPoints.has())
                             nPoints.set(firstStringifiedPoint, nPoints.get(firstStringifiedPoint) + 1);
@@ -31,13 +36,19 @@ geoJSONparser = {
                         //add connecting point (first point must remain and have the same coordinates as last point).
                         pointsLeft.add(firstStringifiedPoint);
                         let iter = 0;
-                        for(let point of polygonCordinates){
+                        let localOptimizationFactor;
+                        if(polygonCordinates[0].length/optimizationFactor <= 4)
+                            localOptimizationFactor = polygonCordinates[0].length / 5;
+                        else
+                            localOptimizationFactor = optimizationFactor;
+
+                        for(let point of polygonCordinates[0]){
                             let stringifiedPoint = String(point);
                             if(nPoints.has(stringifiedPoint))
                                 nPoints.set(stringifiedPoint, nPoints.get(stringifiedPoint) + 1);
                             else
                                 nPoints.set(stringifiedPoint, 1);
-                            if(iter >= optimizationFactor){
+                            if(iter >= localOptimizationFactor){
                                 iter = 0;
                                 pointsLeft.add(stringifiedPoint);
                             }
@@ -47,23 +58,30 @@ geoJSONparser = {
                 }
             }
             //add nPoints where n > 2
-            console.log(nPoints.entries());
+            //console.log(nPoints.entries());
             for(let point of nPoints) {
                 if(point[1] > 2){
-                    console.log("mPoint: " + point[0] +"num:" + point[1]);
+                    //console.log("mPoint: " + point[0] +"num:" + point[1]);
                     pointsLeft.add(point[0]);
                 }
             }
 
             for(let feature of geoJson.features){
                 if(feature.geometry.type == 'Polygon' || feature.geometry.type == 'MultiPolygon'){
-                    for(let i = 0; i < feature.geometry.coordinates.length; i++){
-                        let coordinates = feature.geometry.coordinates[i];
-                        feature.geometry.coordinates[i] = [];
+                    let encapsulate;
+                    if(feature.geometry.type == 'Polygon')
+                        encapsulate = [feature.geometry.coordinates];
+                    else
+                        encapsulate = feature.geometry.coordinates;
+                    for(let i = 0; i < encapsulate.length; i++){
+                        //console.log(i);
+                        let coordinates = encapsulate[i][0];
+                        encapsulate[i][0] = [];
+                        //console.log(coordinates[2]);
                         for(let point of coordinates){
-                            //console.log(point);
+                            //console.log(i);
                             if(pointsLeft.has(String(point))){
-                                feature.geometry.coordinates[i].push(point);
+                                encapsulate[i][0].push(point);
                             }
                         }
                     }
