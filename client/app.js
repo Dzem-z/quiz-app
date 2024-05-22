d843e7acb96e083cd61a95cdaa65f2d6146dbd73
WiadroMasla <tomasz.kosmulski@student.uj.edu.pl>
2024-05-22 10:50:19 +0200

    Loading Screen

--- client/app.js
+++ client/app.js
@@ -1,8 +1,13 @@
 import { DATA_FETCHER } from "./dataFetcher.js";
 import { MapLoader } from "./mapLoader.js";
 import { createWorld } from "./world.js";
+import { loadingScreenSwapper } from "./loadingScreenSwapper.js";
 
+loadingScreenSwapper.startLoad();
 const map = L.map('map');
+//L.control.scale().addTo(map);
+//map.setMinZoom(0);
+//map.setMaxZoom(8);
 const mapLoader = new MapLoader(map);
 const world = await createWorld();
 mapLoader.loadChooser(world);
@@ -12,4 +17,4 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-alert("Data loaded!");
+loadingScreenSwapper.endLoad();
